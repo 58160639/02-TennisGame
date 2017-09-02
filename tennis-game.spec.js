4e00d65e8b8136760bad3a1df0a12cd7c836b62d
Your Name <58160639@go.buu.ac.th>
2017-09-02 15:57:32 +0700

    PlayerA and PlayerB

--- tennis-game.spec.js
+++ tennis-game.spec.js
@@ -1,27 +1,43 @@
 function TennisGame(){
-  this.reset = () =>{
-  }
-    this.echo = () => {
-      return 'Love - Love'
 
-  }
+  const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
+this.playerAScore = 0
+
+this.reset = () => {
+    this.playerAScore = 0
 }
 
-test ('Echo "Love - Love" when press reset botton', () => {
-  //arrange เตรียม input
-  let app = new TennisGame()
+this.echo = () => {
+   return `${scoreString[this.playerAScore]} - Love`
+}
+
+this.playerAGteScore = () => {
+    this.playerAScore++
+}
+}
+
+test('Echo "Love - Love" when game start', () => {
+   // Arrange
+   let app = new TennisGame()
+
+   // Act
+   app.reset()
+   let result = app.echo()
+
+   // Assert
+   expect(result).toBe('Love - Love')
+})
 
 
+test('Echo "Fifteen - Love" when playerA get first score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerAGteScore()
 
-  //act
-  app.reset()
-  let result = app.echo()
+    // Act
+    let result = app.echo()
 
-
-
-
-  //assert output
-  expect(result).toBe('Love - Love')
-
-
+    // Assert
+    expect(result).toBe('Fifteen - Love')
 })
